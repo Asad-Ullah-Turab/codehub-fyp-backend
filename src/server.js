3a6864dfcdbcf4f0c1e3578b781d2c76bd5979c9
israrahmad <israrahmad2004.aa@gmail.com>
2025-09-23 19:10:04 +0500

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const app = require("./app");
+
+require("dotenv").config();
+
+const PORT = process.env.PORT || 4000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
