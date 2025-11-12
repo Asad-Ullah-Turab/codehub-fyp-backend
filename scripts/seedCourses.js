31ce1b6dacc7f644eb6890b61acaeedd9efc3782
asad-ullah-turab <asadullahturab47@gmail.com>
2025-11-13 02:00:41 +0500

    added seed courses code

--- scripts/seedCourses.js
+++ scripts/seedCourses.js
@@ -1,8 +1,14 @@
-import seedCourses from "../utils/courseSeedData.js";
+import seedCourses from "../src/utils/courseSeedData.js";
+import connectDB from "../src/config/database.js";
 
 // Script to seed courses
 const runSeed = async () => {
   try {
+    // Connect to database
+    await connectDB();
+    console.log("📡 Connected to database");
+    
+    // Run seeding
     await seedCourses();
     console.log("✨ Seeding completed successfully!");
     process.exit(0);
