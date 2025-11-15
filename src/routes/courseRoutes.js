f2f428fd7b40f92a4589f26b292215a1eadf1d71
asad-ullah-turab <asadullahturab47@gmail.com>
2025-11-15 19:47:19 +0500

    fixed courses enrollmmnt errrror

--- src/routes/courseRoutes.js
+++ src/routes/courseRoutes.js
@@ -7,7 +7,14 @@ const router = express.Router();
 // ========== PUBLIC ROUTES ==========
 router.get("/", courseController.getAllCourses);
 router.get("/language/:language", courseController.getCoursesByLanguage);
-router.get("/:id", courseController.getCourseById);
+// Use auth middleware optionally - will attach user if token exists, but won't fail without it
+router.get("/:id", (req, res, next) => {
+  const token = req.headers.authorization?.split(' ')[1];
+  if (token) {
+    return auth(req, res, next);
+  }
+  next();
+}, courseController.getCourseById);
 
 // ========== PROTECTED ROUTES - ENROLLMENT ==========
 router.post("/enroll", auth, courseController.enrollInCourse);
